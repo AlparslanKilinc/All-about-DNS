814e6569593294df9083ed06637246c356e2d38d
Alparslan Kilinc <kilincalparslan70@gmail.com>
2023-02-19 12:49:00 -0500

    add dig_down function

---
+++ test.py
@@ -0,0 +1,60 @@
+# You will use two APIs to create a DNS request to each individual server. The first is to
+# create a DNS query and the second is to send this query to the destination. Figuring out
+# the right APIs is up to you, but both can be found in the library. However, as mentioned
+# earlier you are not allowed to use the resolve function in the library.
+
+# Take input domain name 
+# contact root server
+# contact top-level- domain
+# authoritative name server
+
+# Resolve the "A" record for the query.
+
+# Return Errors
+import dns.message
+import dns.rdataclass
+import dns.rdatatype
+import dns.query
+
+qname = dns.name.from_text("yahoo.com")
+query = dns.message.make_query(qname, dns.rdatatype.A)
+response = dns.query.udp(query, "198.41.0.4")
+
+
+
+
+def dig_down(response):
+    # check for answer
+    if response.answer:
+        return response.answer
+    # dig for answer
+    if response.additional:
+        for i in range(len(response.additional)):
+            type_A= str(response.additional[i]).split()[3]
+            if type_A == "A":
+                ip= str(response.additional[i]).split()[4]
+                break
+        new_response=dns.query.udp(query, ip)
+        return dig_down(new_response)
+    else:
+        #have to check cName
+        print("omg")
+        if response.authority:
+            cName=str(response.authority[0]).split()[0]
+            qName= dns.name.from_text(cName)
+            que = dns.message.make_query(qName, dns.rdatatype.A)
+            response = dns.query.udp(que, "198.41.0.4")
+            return dig_down(response)
+    return ""
+
+
+print("QUESTION SECTION:")
+print(response.question[0])
+print("Answer Section:")
+answer_list=dig_down(response)
+for ans in answer_list:
+    print(ans)
+
+
+
+
