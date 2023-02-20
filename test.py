66c3330535daf4a9896ee4b174dfa3f0b85f1097
Alparslan Kilinc <kilincalparslan70@gmail.com>
2023-02-20 13:25:18 -0500

    fix authorative issue , still netflix issue

--- test.py
+++ test.py
@@ -16,11 +16,11 @@ import dns.rdataclass
 import dns.rdatatype
 import dns.query
 
-qname = dns.name.from_text("www.yahoo.com")
-query = dns.message.make_query(qname, dns.rdatatype.A)
-response = dns.query.udp(query, "199.9.14.201")
+initial_qname = dns.name.from_text("amazon.com")
+initial_query = dns.message.make_query(initial_qname, dns.rdatatype.A)
+initial_response = dns.query.udp(initial_query, "199.9.14.201")
 
-def dig_down(response,query_g):
+def dig_down(response,query):
     ip=""
     if response.answer:
         return response.answer
@@ -33,8 +33,8 @@ def dig_down(response,query_g):
                 break
         if ip=="":
             return "Error"
-        new_response=dns.query.udp(query_g, ip)
-        return dig_down(new_response,query_g)
+        new_response=dns.query.udp(query, ip)
+        return dig_down(new_response,query)
 
     if response.authority:
         ip=""
@@ -42,26 +42,19 @@ def dig_down(response,query_g):
         qname_a = dns.name.from_text(aName)
         query_a = dns.message.make_query(qname_a, dns.rdatatype.A)
         response_a = dns.query.udp(query_a, "199.9.14.201")
-        print(response_a)
-        if response_a.additional:
-            for i in range(len(response_a.additional)):
-                type_A= str(response_a.additional[i]).split()[3]
-                if type_A == "A":
-                    ip= str(response_a.additional[i]).split()[4]
-                    break
-        if ip=="":
-            return "Error"
-        a_response=dns.query.udp(query_a, ip)
-        return dig_down(a_response,query_g)
+        answer=dig_down(response_a,query_a)
+        ip=str(answer[0]).split()[4]
+        new_resp=dns.query.udp(query, ip)
+        return dig_down(new_resp,query)
 
     return "Error"
 
 
 
 print("QUESTION SECTION:")
-print(response.question[0])
+print(initial_response.question[0])
 print("Answer Section:")
-answer=dig_down(response,query)[0]
+answer=dig_down(initial_response,initial_query)[0]
 if answer == "E":
     print("ERROR")
 else:
