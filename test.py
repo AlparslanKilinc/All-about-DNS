143b7224d19f3c22baf05da8d04fee195f28c72c
Alparslan Kilinc <kilincalparslan70@gmail.com>
2023-02-21 15:26:57 -0500

    final commit before submission

--- test.py
+++ test.py
@@ -30,37 +30,33 @@ initial_response = dns.query.udp(initial_query, "199.9.14.201")
 # If No Answer or Additional Then check Authority: Authority Algo Extracts the server name to be asked then asks it to the Root
 # Inside of Authority we check if we have an Answer if not we dig_down with our authority query to get IP address then using this IP
 # we ask our actual query to this IP (Not the Authoritative query) , if we have answer we return other wise we keep digging.
-def dig_down(response,query):
-    ip=""
+def dig_down(response,query,ip):
     if response.answer:
         return response.answer
-
     if response.additional:
         for i in range(len(response.additional)):
             type_A= str(response.additional[i]).split()[3]
             if type_A == "A":
                 ip= str(response.additional[i]).split()[4]
                 break
-        if ip=="":
-            return "Error"
         new_response=dns.query.udp(query, ip)
-        return dig_down(new_response,query)
+        return dig_down(new_response,query,ip)
 
     if response.authority:
-        ip=""
         aName=str(response.authority[0]).split()[4]
         qname_a = dns.name.from_text(aName)
         query_a = dns.message.make_query(qname_a, dns.rdatatype.A)
         response_a = dns.query.udp(query_a, "199.9.14.201")
         if response_a.answer:
             return response_a.answer
-        answer=dig_down(response_a,query_a)
-        ip=str(answer[0]).split()[4]
-        new_resp=dns.query.udp(query, ip)
-        if new_resp.answer:
-            if str(new_resp.answer[0]).split()[3] == "A":
-                return new_resp.answer
-        return dig_down(new_resp,query)
+        ip_place = dig_down(response_a,query_a,"199.9.14.201")
+        ip_2=str(ip_place[0]).split()[4]
+        if ip == ip_2:
+            return ip_place
+        response_deep=dns.query.udp(query, ip_2)
+        if response_deep.answer:
+            return response_deep.answer
+        return dig_down(response_deep,query,ip)
     return "Error"
 
 
@@ -70,7 +66,7 @@ print(initial_response.question[0],file=file1)
 print("",file=file1)
 print("Answer Section:",file=file1)
 # Making our Initial Dig 
-answer=dig_down(initial_response,initial_query)[0]
+answer=dig_down(initial_response,initial_query,"199.9.14.201")[0]
 # checking if Initial Dig Returned Error
 if answer == "E":
     print("ERROR",file=file1)
@@ -83,7 +79,7 @@ while answer != "E" and str(answer).split()[3]=="CNAME":
     query_c = dns.message.make_query(qname_c, dns.rdatatype.A)
     response_c = dns.query.udp(query_c,"199.9.14.201")
     # Using Cname to dig. If we get Answer that is not CNAME it will exit 
-    answer= dig_down(response_c,query_c)[0]
+    answer= dig_down(response_c,query_c,"199.9.14.201")[0]
     # Checking for error , print then exits loop
     if answer == "E":
         print("ERROR",file=file1)
